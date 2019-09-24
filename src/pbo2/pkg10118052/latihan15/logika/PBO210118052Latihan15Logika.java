89708b37fdc27b3fdeb8edb0f9406c800826b3ec
Bagus Syibro Malisi <55564197+syibro98@users.noreply.github.com>
2019-09-24 12:48:13 +0700

    Add files via upload

---
+++ src/pbo2/pkg10118052/latihan15/logika/PBO210118052Latihan15Logika.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10118052.latihan15.logika;
+
+/**
+ * @author 
+ * NAMA      : Bagus Syibro Malisi
+ * KELAS     : PBO2
+ * NIM       : 10118052
+ * Deskripsi Program : Latihan ini bertujuan untuk belajar operator logika
+ */
+public class PBO210118052Latihan15Logika {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        boolean a = true;
+        boolean b = false;
+        System.out.println("a && b = " + (a && b));
+        System.out.println("a || b = " + (a || b));
+        System.out.println("!a = " + !a );
+        System.out.println("!(a && b) = " + !(a && b));
+    }
+    
+}
